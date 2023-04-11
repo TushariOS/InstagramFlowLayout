9bbccd0e29e7cc3b65bb72f31081961ebfac3775
JTushar <tushar@heady.io>
2023-04-11 21:48:42 +0530

    Initial Commit

---
+++ CompositionalLayoutWithSwiftUI/CompositionalLayoutWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompositionalLayoutWithSwiftUIApp.swift
+//  CompositionalLayoutWithSwiftUI
+//
+//  Created by Tushar Jaunjalkar on 11/04/23.
+//
+
+import SwiftUI
+
+@main
+struct CompositionalLayoutWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
